9ad654645b8973be2835513fb45bcd19ac52e94b
Prithibee13 <66945275+Prithibee13@users.noreply.github.com>
2022-04-29 01:27:35 +0600

    db connection

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const cors = require('cors');
 
 const app = express();
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
 const port = process.env.PORT || 8000;
 
 app.use(cors())
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.poftg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 app.listen(port , ()=>
