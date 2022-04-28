f05728014927936807a3f01f4b16d8ae920e012b
Prithibee13 <66945275+Prithibee13@users.noreply.github.com>
2022-04-28 22:41:10 +0600

    Port installation

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const cors = require('cors');
 
 const app = express();
 
+const port = process.env.PORT || 8000;
 
 app.use(cors())
 app.use(express.json())
+
+
+
+app.listen(port , ()=>
+{
+    console.log("Fruit-server is runnig in", port);
+})
